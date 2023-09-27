4452fbbf9206efeb144f75c193c9ff50e692f1a3
Carl1131 <carlsantos31@yahoo.com>
2023-09-27 22:03:12 +1300

    Feat: Edited the backend Jest test. Desc: The backend Jest unit test now passes.

--- speed_backend/src/app.controller.spec.ts
+++ speed_backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! From back-end."', () => {
+      expect(appController.getHello()).toBe('Hello World! From back-end.');
     });
   });
 });
